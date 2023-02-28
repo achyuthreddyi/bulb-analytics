526cfce9e103696f2dbd2547defc35a4cd796be6
achyuthreddy <ncachyuth@gmail.com>
2023-02-28 21:55:09 +0530

    feat: Adds calculate bulb glow method for 2 consecutive data points with no missing data

--- bulb_analytics.py
+++ bulb_analytics.py
@@ -1,2 +1,16 @@
-def calculate_bulb_analytics():
-    return 'testing'
+def calculate_bulb_glow_duration(data_point_1, data_point_2):
+    if data_point_1['data'] == 1 and data_point_2['data'] == 1:
+        return data_point_2['timestamp'] - data_point_1['timestamp']
+    return 0
+
+def calculate_bulb_analytics(data_stream, sliding_window_size):
+    data_stream_length = len(data_stream)
+    total_duration = 0
+    sliding_window = sorted(data_stream[0:sliding_window_size], key=lambda i: i['timestamp'])
+
+    # if data_stream_length < sliding_window_size:
+    #     for i in range (0, len(sliding_window) - 1):
+    #         total_duration +=
+
+
+
--- test_bulb_analytics.py
+++ test_bulb_analytics.py
@@ -2,6 +2,29 @@ from unittest import TestCase
 from bulb_analytics import *
 
 
-class Test(TestCase):
+class CalculateBulbTest(TestCase):
     def test_calculate_bulb_analytics(self):
         self.assertEqual(calculate_bulb_analytics(), 'testing')
+
+class CalculateBulbGlowDuration(TestCase):
+    # test cases for the consecutive data points with no missing data points in between
+    def test_calculate_bulb_glow_duration_when_2_datapoints_positive(self):
+        data_point_1 = {'timestamp': 1, 'data': 1}
+        data_point_2 = {'timestamp': 2, 'data': 1}
+
+        self.assertEqual(calculate_bulb_glow_duration(data_point_1, data_point_2), 1)
+
+    def test_calculate_bulb_glow_duration_when_2_datapoints_negative(self):
+        data_point_1 = {'timestamp': 1, 'data': 0}
+        data_point_2 = {'timestamp': 2, 'data': 0}
+
+        self.assertEqual(calculate_bulb_glow_duration(data_point_1, data_point_2), 0)
+
+    def test_calculate_bulb_glow_duration_when_1_datapoint_negative(self):
+        data_point_1 = {'timestamp': 1, 'data': 1}
+        data_point_2 = {'timestamp': 2, 'data': 0}
+
+        self.assertEqual(calculate_bulb_glow_duration(data_point_1, data_point_2), 0)
+
+
+

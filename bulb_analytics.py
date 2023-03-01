394b79ee942dbf1381c10cf3e9cefac933b62c41
achyuthreddy <ncachyuth@gmail.com>
2023-03-01 16:01:46 +0530

    feat:adds calculate bulb analytics method for no missing data

--- bulb_analytics.py
+++ bulb_analytics.py
@@ -1,35 +1,51 @@
-def calculate_bulb_glow_duration(data_point_1, data_point_2, timestamps):
-    print('error in here')
-    if (len(timestamps)) > 0 and (timestamps[len(timestamps) - 1]['to'] == data_point_1['timestamp']):
-        timestamps_last_index = len(timestamps) - 1
-        timestamps[timestamps_last_index]['to'] = data_point_2['timestamp']
-        timestamps[timestamps_last_index]['duration'] = data_point_2['timestamp'] - timestamps[timestamps_last_index][
+def update_array(timestamps, data_point_1, data_point_2):
+    timestamps_length = len(timestamps)
+
+    if timestamps_length > 0 and timestamps[timestamps_length - 1]['to'] == data_point_1['timestamp']:
+        timestamps[timestamps_length - 1]['to'] = data_point_2['timestamp']
+        timestamps[timestamps_length - 1]['duration'] = data_point_2['timestamp'] - timestamps[timestamps_length - 1][
             'from']
     else:
         timestamps.append({'from': data_point_1['timestamp'], 'to': data_point_2['timestamp'],
                            'duration': data_point_2['timestamp'] - data_point_1['timestamp']})
-    print ({'duration': data_point_2['timestamp'] - data_point_1['timestamp'],
-            'timestamps': timestamps})
-    return {'duration': data_point_2['timestamp'] - data_point_1['timestamp'],
-            'timestamps': timestamps}
+    return timestamps
+
+
+def calculate_bulb_glow_duration_1(data_point_1, data_point_2, timestamps):
+    bulb_on_timestamps = {'total_duration': 0, 'timestamps': []}
+
+    if data_point_1['data'] == 1:
+        bulb_on_timestamps['total_duration'] += data_point_2['timestamp'] - data_point_1['timestamp']
+        bulb_on_timestamps['timestamps'] = update_array(timestamps, data_point_1, data_point_2)
+    return bulb_on_timestamps
 
 
 def calculate_bulb_analytics(data_stream, sliding_window_size):
-    data_stream_length = len(data_stream)
+    data_stream_len = len(data_stream)
+    sliding_window = sorted(data_stream[0:sliding_window_size], key=lambda i: i['timestamp'])
+    counter_timestamp = sliding_window[0]['timestamp']
     total_bulb_on_duration = 0
     timestamps = []
-    sliding_window = sorted(data_stream[0:sliding_window_size], key=lambda i: i['timestamp'])
 
-    for i in range(sliding_window_size, data_stream_length):
+    for i in range(sliding_window_size, data_stream_len):
+        print('counter_timestamp', counter_timestamp)
         popped_element = sliding_window.pop(0)
         sliding_window.append(data_stream[i])
-        # TODO: Need not sort everytime rather you can place the new element in its place
         sliding_window = sorted(sliding_window, key=lambda j: j['timestamp'])
 
-        if popped_element['data'] == 1 and sliding_window[0]['data'] == 1:
-            result = calculate_bulb_glow_duration(popped_element, sliding_window[0], timestamps)
-            total_bulb_on_duration += result['duration']
+        if popped_element['timestamp'] == counter_timestamp:
+
+            if sliding_window[0]['data'] == popped_element['data']:
+                result = calculate_bulb_glow_duration_1(popped_element, sliding_window[0], timestamps)
+                total_bulb_on_duration += result['total_duration']
+                timestamps = result['timestamps']
+        else:
+            new_data_point = {'timestamp': sliding_window[0]['timestamp'] - 1, 'data': popped_element['data']}
+            print('new_data_point', new_data_point)
+            result = calculate_bulb_glow_duration_1(popped_element, new_data_point, timestamps)
+            total_bulb_on_duration += result['total_duration']
             timestamps = result['timestamps']
+            counter_timestamp = sliding_window[0]['timestamp']
 
+        counter_timestamp += 1
     return {'total_bulb_on_duration': total_bulb_on_duration, 'timestamps': timestamps}
-
--- test_bulb_analytics.py
+++ test_bulb_analytics.py
@@ -2,46 +2,46 @@ from unittest import TestCase
 from bulb_analytics import *
 
 
-class CalculateBulbGlowDuration(TestCase):
-    # test cases for the consecutive data points with no missing data points in between
-    def test_calculate_bulb_glow_duration_when_2_consecutive_datapoints_positive(self):
-        data_point_1 = {'timestamp': 1, 'data': 1}
-        data_point_2 = {'timestamp': 2, 'data': 1}
-        timestamps = []
-
-        expected_timestamps = [{'from': 1, 'to': 2}]
-
-        self.assertEqual(calculate_bulb_glow_duration(data_point_1, data_point_2, timestamps),
-                         {'duration': 1, 'timestamps': expected_timestamps})
-
-    def test_calculate_bulb_glow_duration_when_2_discrete_datapoints_positive(self):
-        data_point_1 = {'timestamp': 1, 'data': 1}
-        data_point_2 = {'timestamp': 5, 'data': 1}
-        timestamps = []
-
-        expected_timestamps = [{'from': 1, 'to': 5}]
-        self.assertEqual(calculate_bulb_glow_duration(data_point_1, data_point_2, timestamps),
-                         {'duration': 4, 'timestamps': expected_timestamps})
+# class CalculateBulbGlowDuration(TestCase):
+#     # test cases for the consecutive data points with no missing data points in between
+#     def test_calculate_bulb_glow_duration_when_2_consecutive_datapoints_positive(self):
+#         data_point_1 = {'timestamp': 1, 'data': 1}
+#         data_point_2 = {'timestamp': 2, 'data': 1}
+#         timestamps = []
+#
+#         expected_timestamps = [{'from': 1, 'to': 2}]
+#
+#         self.assertEqual(calculate_bulb_glow_duration(data_point_1, data_point_2, timestamps),
+#                          {'duration': 1, 'timestamps': expected_timestamps})
+#
+#     def test_calculate_bulb_glow_duration_when_2_discrete_datapoints_positive(self):
+#         data_point_1 = {'timestamp': 1, 'data': 1}
+#         data_point_2 = {'timestamp': 5, 'data': 1}
+#         timestamps = []
+#
+#         expected_timestamps = [{'from': 1, 'to': 5}]
+#         self.assertEqual(calculate_bulb_glow_duration(data_point_1, data_point_2, timestamps),
+#                          {'duration': 4, 'timestamps': expected_timestamps})
 
 
 class CalculateBulbAnalytics(TestCase):
-    def test_calculate_bulb_analytics_with_ordered_and_no_missing_data(self):
-        data_list = [{'timestamp': 1, 'data': 1}, {'timestamp': 2, 'data': 1},
-                    {'timestamp': 3, 'data': 1}, {'timestamp': 4, 'data': 0},
-                    {'timestamp': 5, 'data': 1}, {'timestamp': 6, 'data': 1},
-                    {'timestamp': 7, 'data': 1}, {'timestamp': 8, 'data': 0},
-                    {'timestamp': 9, 'data': 0}]
+    def test_calculate_bulb_analytics_with_ordered_and_no_positive_data(self):
+        data_list = [{'timestamp': 1, 'data': 0}, {'timestamp': 2, 'data': 0},
+                     {'timestamp': 3, 'data': 0}, {'timestamp': 4, 'data': 0},
+                     {'timestamp': 5, 'data': 0}, {'timestamp': 6, 'data': 0},
+                     {'timestamp': 7, 'data': 0}, {'timestamp': 8, 'data': 0},
+                     {'timestamp': 9, 'data': 0}]
         window_size = 3
 
-        result = {'total_bulb_on_duration': 4,
-                  'timestamps': [{'from': 1, 'to': 3, 'duration': 2},
-                                 {'from': 5, 'to': 7, 'duration': 2}]}
+        result = {'total_bulb_on_duration': 0,
+                  'timestamps': []}
 
         self.assertEqual(calculate_bulb_analytics(data_list, window_size), result)
 
-    def test_calculate_bulb_analytics_with_ordered_and_missing_data(self):
-        data_list = [{'timestamp': 1, 'data': 1},{'timestamp': 3, 'data': 1},
-                     {'timestamp': 4, 'data': 0}, {'timestamp': 5, 'data': 1},
+    def test_calculate_bulb_analytics_with_ordered_and_no_missing_data(self):
+        data_list = [{'timestamp': 1, 'data': 1}, {'timestamp': 2, 'data': 1},
+                     {'timestamp': 3, 'data': 1}, {'timestamp': 4, 'data': 0},
+                     {'timestamp': 5, 'data': 1}, {'timestamp': 6, 'data': 1},
                      {'timestamp': 7, 'data': 1}, {'timestamp': 8, 'data': 0},
                      {'timestamp': 9, 'data': 0}]
         window_size = 3
@@ -52,13 +52,41 @@ class CalculateBulbAnalytics(TestCase):
 
         self.assertEqual(calculate_bulb_analytics(data_list, window_size), result)
 
-    def test_calculate_bulb_analytics_with_unordered_and_missing_data(self):
-        data_list = [{'timestamp': 3, 'data': 1}, {'timestamp': 1, 'data': 1}]
+    def test_calculate_bulb_analytics_with_unordered_and_no_missing_data(self):
+        data_list = [{'timestamp': 1, 'data': 1}, {'timestamp': 3, 'data': 1},
+                     {'timestamp': 2, 'data': 1}, {'timestamp': 4, 'data': 0},
+                     {'timestamp': 5, 'data': 1}, {'timestamp': 7, 'data': 1},
+                     {'timestamp': 6, 'data': 1}, {'timestamp': 8, 'data': 0},
+                     {'timestamp': 9, 'data': 0}]
         window_size = 3
 
-        result = {'total_bulb_on_duration': 2,
-                  'timestamps': [{'from': 1, 'to': 3, 'duration': 2}]}
+        result = {'total_bulb_on_duration': 4,
+                  'timestamps': [{'from': 1, 'to': 3, 'duration': 2},
+                                 {'from': 5, 'to': 7, 'duration': 2}]}
 
         self.assertEqual(calculate_bulb_analytics(data_list, window_size), result)
 
+    # def test_calculate_bulb_analytics_with_ordered_and_missing_data(self):
+    #     data_list = [{'timestamp': 1, 'data': 1}, {'timestamp': 3, 'data': 1},
+    #                  {'timestamp': 4, 'data': 0}, {'timestamp': 5, 'data': 1},
+    #                  {'timestamp': 7, 'data': 1}, {'timestamp': 8, 'data': 0},
+    #                  {'timestamp': 9, 'data': 0}]
+    #     window_size = 3
+    #
+    #     result = {'total_bulb_on_duration': 4,
+    #               'timestamps': [{'from': 1, 'to': 3, 'duration': 2},
+    #                              {'from': 5, 'to': 7, 'duration': 2}]}
+    #
+    #     self.assertEqual(calculate_bulb_analytics(data_list, window_size), result)
 
+    # def test_calculate_bulb_analytics_with_unordered_and_missing_data(self):
+    #     data_list = [{'timestamp': 1, 'data': 1}, {'timestamp': 7, 'data': 1},
+    #                 {'timestamp': 3, 'data': 1},
+    #                 {'timestamp': 6, 'data': 0}, {'timestamp': 5, 'data': 1}, {'timestamp': 2, 'data': 0},
+    #                 {'timestamp': 8, 'data': 1}, {'timestamp': 9, 'data': 0}]
+    #     window_size = 10
+    #
+    #     result = {'total_bulb_on_duration': 0,
+    #               'timestamps': []}
+    #
+    #     self.assertEqual(calculate_bulb_analytics(data_list, window_size), result)
